2e12ddc758af93df45f4e33cbe2d9dabe566aae1
IJustDev <alexanderpanovi@yahoo.de>
2019-11-15 08:02:59 +0100

    Added working deploy script to travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,14 +14,6 @@ build () {
   docker push $CONTAINER_TAG
 }
 
-deployToServer () {
-  echo $SSH_KEY > ~/.ssh/id_rsa
-  ssh-add ~/.ssh/id_rsa
-  ssh-keyscan -H $DEPLOY_IP >> ~/.ssh/known_hosts
-  ssh $DEPLOY_USER@$DEPLOY_IP "docker pull $CONTAINER_TAG \ echo 'asdf'"
-}
-
 build
-deployToServer
 
 exit 0
